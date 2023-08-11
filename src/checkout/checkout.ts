eea1da5409d5f245ffbae885388e66a13c0f5070
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-08-11 17:53:32 +0100

    refactor(checkout-kata): Simplify code logic for calculating total price

--- src/checkout/checkout.ts
+++ src/checkout/checkout.ts
@@ -5,44 +5,58 @@ enum Item {
     D = 'D',
 }
 
-type Product = {
-    item: Item;
-    price: number;
-    specialOffer?: { quantity: number; offerPrice: number };
-};
+interface SpecialOffer {
+    quantity: number
+    offerPrice: number
+}
+
+interface Product {
+    item: Item
+    price: number
+    specialOffer?: SpecialOffer
+}
 
 const products: Product[] = [
-    { item: Item.A, price: 50, specialOffer: { quantity: 3, offerPrice: 130 } },
-    { item: Item.B, price: 30, specialOffer: { quantity: 2, offerPrice: 45 } },
-    { item: Item.C, price: 20 },
-    { item: Item.D, price: 15 },
+    {item: Item.A, price: 50, specialOffer: {quantity: 3, offerPrice: 130}},
+    {item: Item.B, price: 30, specialOffer: {quantity: 2, offerPrice: 45}},
+    {item: Item.C, price: 20},
+    {item: Item.D, price: 15},
 ];
 
+const validateItem = (item: string): void => {
+    if (!Object.values(Item).includes(item as Item)) {
+        throw new Error('Invalid product');
+    }
+}
+
+const calculateTotalPrice = (product: Product, quantity: number): number => {
+    const {price, specialOffer} = product
+
+    if (specialOffer && quantity >= specialOffer.quantity) {
+        const specialOfferCount = Math.floor(quantity / specialOffer.quantity)
+        const remainingItems = quantity % specialOffer.quantity
+        return specialOfferCount * specialOffer.offerPrice + remainingItems * price
+    }
+
+    return quantity * price
+}
+
 export const checkout = (basket: string): number => {
-    const basketItems = basket.split('');
-
-    const countItems = new Map<Item, number>();
+    const basketItems = basket.split('')
+    const countItems = new Map<Item, number>()
     basketItems.forEach(item => {
-        if (!Object.values(Item).includes(item as Item)) {
-            throw new Error('Invalid product');
-        }
-        countItems.set(item as Item, (countItems.get(item as Item) || 0) + 1);
-    });
+        validateItem(item);
+        const itemKey = item as Item;
+        countItems.set(itemKey, (countItems.get(itemKey) || 0) + 1)
+    })
+    let totalPrice = 0
 
-    let totalPrice = 0;
     for (const [item, quantity] of countItems.entries()) {
-        const product = products.find(p => p.item === item);
+        const product = products.find(p => p.item === item)
         if (product) {
-            const { price, specialOffer } = product;
-            if (specialOffer && quantity >= specialOffer.quantity) {
-                const specialOfferCount = Math.floor(quantity / specialOffer.quantity);
-                const remainingItems = quantity % specialOffer.quantity;
-                totalPrice += specialOfferCount * specialOffer.offerPrice + remainingItems * price;
-            } else {
-                totalPrice += quantity * price;
-            }
+            totalPrice += calculateTotalPrice(product, quantity)
         }
     }
 
-    return totalPrice;
-};
+    return totalPrice
+}
