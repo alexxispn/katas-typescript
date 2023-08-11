88ecb8324d5675351b6c17a014b619fba55351fe
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-08-11 13:13:01 +0100

    feat(checkout-kata): Handle single-letter product codes, returning respective prices

--- src/checkout/checkout.spec.ts
+++ src/checkout/checkout.spec.ts
@@ -1,7 +1,16 @@
 import {checkout} from "./checkout";
 
 describe('checkout', () => {
-    it('should return true', () => {
-        expect(checkout()).toBe(true)
+    it('should return 50 when we passed one A', () => {
+        expect(checkout('A')).toBe(50)
     })
+    it('should return 30 when we passed one B', () => {
+        expect(checkout('B')).toBe(30)
+    });
+    it('should return 20 when we passed one C', () => {
+        expect(checkout('C')).toBe(20)
+    });
+    it('should return 15 when we passed one D', () => {
+        expect(checkout('D')).toBe(15)
+    });
 })
--- src/checkout/checkout.ts
+++ src/checkout/checkout.ts
@@ -1,4 +1,8 @@
-export const checkout = () => {
-    return true
+// @ts-ignore
+export const checkout = (product: string): number => {
+    if (product === 'A') return 50
+    if (product === 'B') return 30
+    if (product === 'C') return 20
+    return 15
 }
 
