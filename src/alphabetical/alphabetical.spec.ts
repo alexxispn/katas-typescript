765f73b426cc5f9a288715540c55d0d5614b5735
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-08-12 19:07:46 +0100

    test(add-all-numbers-kata): Test multiple-string array scenario

--- src/alphabetical/alphabetical.spec.ts
+++ src/alphabetical/alphabetical.spec.ts
@@ -7,4 +7,10 @@ describe('alphabetical', () => {
     it('should return a list with one element when receives a list with one element', () => {
         expect(alphabeticalSort(['a'])).toEqual(['a']);
     });
+    it('should return a list with two elements in alphabetical order when receives a list with two elements in reverse alphabetical order', () => {
+        expect(alphabeticalSort(['b', 'a'])).toEqual(['a', 'b']);
+    });
+    it('should return a list with three elements in alphabetical order when receives a list with three elements in reverse alphabetical order', () => {
+        expect(alphabeticalSort(['c', 'b', 'a'])).toEqual(['a', 'b', 'c']);
+    });
 });
