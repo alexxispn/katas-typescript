0b35719e91803e810b2e63b0098ed7bdf599c955
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-08-12 17:42:51 +0100

    test(add-all-numbers-kata): Test single-string array scenario

--- src/alphabetical/alphabetical.spec.ts
+++ src/alphabetical/alphabetical.spec.ts
@@ -4,4 +4,7 @@ describe('alphabetical', () => {
     it('should return an empty list when receives an empty list', () => {
         expect(alphabeticalSort([])).toEqual([]);
     });
+    it('should return a list with one element when receives a list with one element', () => {
+        expect(alphabeticalSort(['a'])).toEqual(['a']);
+    });
 });
