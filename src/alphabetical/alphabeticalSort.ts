9d0e8675662065bdb75bff172e18d7e03bbad646
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-08-12 19:08:05 +0100

    feat(add-all-numbers-kata): Handle multiple-string array scenario

--- src/alphabetical/alphabeticalSort.ts
+++ src/alphabetical/alphabeticalSort.ts
@@ -1,3 +1,15 @@
 export const alphabeticalSort = (list: string[]): string[] => {
+    if (list.length <= 1) return list
+
+    for (let i = 1; i < list.length; i++) {
+        const current = list[i]
+        let j = i - 1
+        while (j >= 0 && list[j] > current) {
+            list[j + 1] = list[j]
+            j--
+        }
+        list[j + 1] = current
+    }
+
     return list
-};
+}
