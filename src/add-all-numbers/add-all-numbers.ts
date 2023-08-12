9ee4b1938ab29606530bd5ab4dd58d32e36f3a6f
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-08-12 16:10:56 +0100

    feat(add-all-numbers-kata): Implement iterative and recursive solutions

--- src/add-all-numbers/add-all-numbers.ts
+++ src/add-all-numbers/add-all-numbers.ts
@@ -1,4 +1,19 @@
-// @ts-ignore
 export const addAllNumbers = (numbers: number[]): number => {
     return numbers.reduce((acc, curr) => acc + curr, 0)
 }
+
+export const addAllNumbersRecursive = (numbers: number[]): number => {
+    if (numbers.length === 0) return 0
+    if (numbers.length === 1) return numbers[0]
+    const [first, ...rest] = numbers
+    return first + addAllNumbersRecursive(rest)
+}
+
+export const addAllNumbersIterative = (numbers: number[]): number => {
+    let sum = 0
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i]
+    }
+    return sum
+}
+
