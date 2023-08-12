f553d77c40c88004c8fc27216ec245be5c28b08c
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-08-12 15:53:09 +0100

    test(add-all-numbers-kata): Test single-number array scenario

--- src/add-all-numbers/add-all-numbers.spec.ts
+++ src/add-all-numbers/add-all-numbers.spec.ts
@@ -4,5 +4,8 @@ describe('addAllNumbers', () => {
     it('should return 0 when receiving an empty array', () => {
         expect(addAllNumbers([])).toBe(0)
     });
+    it('should return the number when there is an array with only one number', () => {
+        expect(addAllNumbers([1])).toBe(1)
+        expect(addAllNumbers([3])).toBe(3)
+    });
 });
-
--- src/add-all-numbers/add-all-numbers.ts
+++ src/add-all-numbers/add-all-numbers.ts
@@ -1,4 +1,5 @@
 // @ts-ignore
 export const addAllNumbers = (numbers: number[]): number => {
+    if (numbers.length === 1) return numbers[0]
     return 0
 }
