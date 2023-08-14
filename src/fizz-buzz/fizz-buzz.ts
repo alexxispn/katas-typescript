d0ba1c6f1a0af8f39ed6136713e91420e07579e4
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-08-14 16:08:44 +0100

    feat(fizzbuzz-kata): Handle invalid input

--- src/fizz-buzz/fizz-buzz.ts
+++ src/fizz-buzz/fizz-buzz.ts
@@ -1,4 +1,10 @@
-// @ts-ignore
+export enum FizzBuzz {
+    FIZZ = 'Fizz',
+    BUZZ = 'Buzz',
+    FIZZ_BUZZ = 'FizzBuzz',
+    ERROR_GIVEN_NUMBER_IS_LESS_THAN_1 = 'Error: Given number is less than 1',
+}
 export const fizzBuzz = (n: number): string => {
+    if (n < 1) return FizzBuzz.ERROR_GIVEN_NUMBER_IS_LESS_THAN_1
     return '1'
 }
