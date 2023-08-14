5aa263cb62c84dc54fe6bf9f57055551efed8552
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-08-14 16:08:16 +0100

    test(fizzbuzz-kata): Test invalid input handling

--- src/fizz-buzz/fizz-buzz.spec.ts
+++ src/fizz-buzz/fizz-buzz.spec.ts
@@ -1,7 +1,10 @@
-import {fizzBuzz} from './fizz-buzz';
+import {fizzBuzz, FizzBuzz} from './fizz-buzz';
 
 describe('fizzBuzz', () => {
     it('should return 1 when 1 is given', () => {
         expect(fizzBuzz(1)).toBe('1');
+    });
+    it('should return an error message when the given number is less than 1', () => {
+        expect(fizzBuzz(0)).toBe(FizzBuzz.ERROR_GIVEN_NUMBER_IS_LESS_THAN_1);
     });
 })
