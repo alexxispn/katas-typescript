07aaae69f846d2921354f085874dc4bdb6e14fde
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-08-14 16:50:49 +0100

    refactor(fizzbuzz-kata): Extract isFizz, isBuzz, isFizzBuzz methods

--- src/fizz-buzz/fizz-buzz.ts
+++ src/fizz-buzz/fizz-buzz.ts
@@ -5,14 +5,15 @@ export enum FizzBuzz {
     ERROR_GIVEN_NUMBER_IS_LESS_THAN_1 = 'Error: Given number is less than 1',
 }
 
+const isFizz = (number: number) => number % 3 === 0
+const isBuzz = (number: number) => number % 5 === 0
+const isFizzBuzz = (number: number) => number % 15 === 0
+
 export const fizzBuzz = (length: number): string[] => {
     if (length < 1) return [FizzBuzz.ERROR_GIVEN_NUMBER_IS_LESS_THAN_1]
     if (length === 1) return ['1']
 
     const numbers = Array.from({length: length}, (_, i) => i + 1)
-    const isFizz = (number: number) => number % 3 === 0
-    const isBuzz = (number: number) => number % 5 === 0
-    const isFizzBuzz = (number: number) => number % 15 === 0
 
     return numbers.map(number => {
         if (isFizzBuzz(number)) return FizzBuzz.FIZZ_BUZZ
