e93235c8fa9096b2e7fe6907c0cd5d6761534be1
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-08-14 15:34:20 +0100

    test(add-all-numbers-kata): Test multiple-string array with same first letter scenario

--- src/alphabetical/alphabetical.spec.ts
+++ src/alphabetical/alphabetical.spec.ts
@@ -10,7 +10,12 @@ describe('alphabetical', () => {
     it('should return a list with two elements in alphabetical order when receives a list with two elements in reverse alphabetical order', () => {
         expect(alphabeticalSort(['b', 'a'])).toEqual(['a', 'b']);
     });
-    it('should return a list with three elements in alphabetical order when receives a list with three elements in reverse alphabetical order', () => {
+    it('should return a list with three elements in alphabetical order when receives a list with three elements in' +
+        ' random alphabetical order', () => {
         expect(alphabeticalSort(['c', 'b', 'a'])).toEqual(['a', 'b', 'c']);
+    });
+    it('should return a list with some words with the same first letter in alphabetical order when receives a list' +
+        ' with some elements with the same first letter in reverse alphabetical order', () => {
+        expect(alphabeticalSort(['antena', 'amor', 'aguacate'])).toEqual(['aguacate', 'amor', 'antena']);
     });
 });
