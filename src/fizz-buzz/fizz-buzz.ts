42ea20c889f5bdc05e8ee9c2126784780c2f884a
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-08-14 16:43:45 +0100

    feat(fizzbuzz-kata): Replace numbers divisible by 15 with 'FizzBuzz'

--- src/fizz-buzz/fizz-buzz.ts
+++ src/fizz-buzz/fizz-buzz.ts
@@ -4,6 +4,7 @@ export enum FizzBuzz {
     FIZZ_BUZZ = 'FizzBuzz',
     ERROR_GIVEN_NUMBER_IS_LESS_THAN_1 = 'Error: Given number is less than 1',
 }
+
 export const fizzBuzz = (length: number): string[] => {
     if (length < 1) return [FizzBuzz.ERROR_GIVEN_NUMBER_IS_LESS_THAN_1]
     if (length === 1) return ['1']
@@ -11,8 +12,10 @@ export const fizzBuzz = (length: number): string[] => {
     const numbers = Array.from({length: length}, (_, i) => i + 1)
     const isFizz = (number: number) => number % 3 === 0
     const isBuzz = (number: number) => number % 5 === 0
+    const isFizzBuzz = (number: number) => number % 15 === 0
 
     return numbers.map(number => {
+        if (isFizzBuzz(number)) return FizzBuzz.FIZZ_BUZZ
         if (isFizz(number)) return FizzBuzz.FIZZ
         if (isBuzz(number)) return FizzBuzz.BUZZ
         return number.toString()
