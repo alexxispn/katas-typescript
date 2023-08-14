70ba9ee7cefdf9be63125466824f9ecb73ac7bad
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-08-14 16:34:06 +0100

    test(fizzbuzz-kata): Test generating a list of numbers up to the given number

--- src/fizz-buzz/fizz-buzz.spec.ts
+++ src/fizz-buzz/fizz-buzz.spec.ts
@@ -2,9 +2,15 @@ import {fizzBuzz, FizzBuzz} from './fizz-buzz';
 
 describe('fizzBuzz', () => {
     it('should return 1 when 1 is given', () => {
-        expect(fizzBuzz(1)).toBe('1');
+        expect(fizzBuzz(1)).toEqual(['1']);
     });
     it('should return an error message when the given number is less than 1', () => {
-        expect(fizzBuzz(0)).toBe(FizzBuzz.ERROR_GIVEN_NUMBER_IS_LESS_THAN_1);
+        expect(fizzBuzz(0)).toEqual([FizzBuzz.ERROR_GIVEN_NUMBER_IS_LESS_THAN_1]);
+    });
+    it('should return 1, 2 when 2 is given', () => {
+        const given = 2;
+
+        const expectedResult = ['1', '2'];
+        expect(fizzBuzz(given)).toEqual(expectedResult)
     });
 })
