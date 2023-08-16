da2411e416b901c6c46c992571754cfdf7b3c64f
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-08-16 16:13:18 +0100

    test(rpg-kata): Ensure default level is set to 1 in Character class

--- src/rpg-kata/Character.spec.ts
+++ src/rpg-kata/Character.spec.ts
@@ -6,4 +6,9 @@ describe('Character', () => {
 
         expect(character.getHealth()).toBe(1000)
     })
+    it('should have level 1 when created', () => {
+        const character = new Character()
+
+        expect(character.getLevel()).toBe(1)
+    })
 })
