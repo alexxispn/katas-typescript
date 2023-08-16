5f63e223bac3a5eea69509b74c393a5fbca15870
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-08-16 16:14:14 +0100

    feat(rpg-kata): Set default level to 1 in Character class

--- src/rpg-kata/Character.ts
+++ src/rpg-kata/Character.ts
@@ -1,8 +1,15 @@
 export class Character {
-    constructor(private health: number = 1000) {
+    constructor(
+        private health: number = 1000,
+        private level: number = 1
+    ) {
     }
 
     getHealth(): number {
         return this.health
+    }
+
+    getLevel(): number {
+        return this.level
     }
 }
