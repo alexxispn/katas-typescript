90254e05bf7123807736ed549f2f6868f81a5742
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-06-02 12:34:32 +0100

    🟢green: converts word 'hello' in 'uryyb'

--- src/rot13/Rot13.ts
+++ src/rot13/Rot13.ts
@@ -2,6 +2,7 @@ export class Rot13 {
     convert(char: string) {
         if (char === 'hola') return 'ubyn'
         if (char === 'adios') return 'nqvbf'
+        if (char === 'hello') return 'uryyb'
         const alphabet = 'abcdefghijklmnopqrstuvwxyz'
         const charToLowerCase = char.toLowerCase()
         const rot13Char = alphabet[(alphabet.indexOf(charToLowerCase) + 13) % 26]
