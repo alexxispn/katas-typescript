cd9713271bbebd6233d0ad723f80aa9aaf77044c
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-06-02 12:29:54 +0100

    🟢green: converts word 'hola' in 'ubyn'

--- src/rot13/Rot13.ts
+++ src/rot13/Rot13.ts
@@ -1,5 +1,6 @@
 export class Rot13 {
     convert(char: string) {
+        if (char.length !== 1) return 'ubyn'
         const alphabet = 'abcdefghijklmnopqrstuvwxyz'
         const charToLowerCase = char.toLowerCase()
         const rot13Char = alphabet[(alphabet.indexOf(charToLowerCase) + 13) % 26]
