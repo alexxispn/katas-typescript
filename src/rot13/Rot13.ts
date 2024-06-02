6b13c665c67f8c2c1b5aeb9cde0cce14dacd9e97
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-06-02 12:22:10 +0100

    🔵refactor: converts second half of alphabet in the char at 13 position to the left

--- src/rot13/Rot13.ts
+++ src/rot13/Rot13.ts
@@ -1,15 +1,6 @@
 export class Rot13 {
     convert(char: string) {
         const alphabet = 'abcdefghijklmnopqrstuvwxyz'
-        if(char === 'n') {
-            return 'a'
-        }
-        if(char === 'o') {
-            return 'b'
-        }
-        if(char === 'p') {
-            return 'c'
-        }
-        return alphabet[alphabet.indexOf(char) + 13]
+        return alphabet[(alphabet.indexOf(char) + 13) % 26]
     }
 }
