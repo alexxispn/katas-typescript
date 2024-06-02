5909c8aefa5d957f604d0a2a42a7e01351912659
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-06-02 12:13:49 +0100

    🔵refactor: add describe in tests

--- src/rot13/Rot13.spec.ts
+++ src/rot13/Rot13.spec.ts
@@ -1,11 +1,13 @@
 import {Rot13} from "./Rot13";
 
 describe("Rot13", () => {
-    it.each([
-      ['a', 'n'],
-      ['b', 'o'],
-      ['c', 'p'],
-    ])('converts char %s in %s', (char, rot13Char) => {
-        expect(new Rot13().convert(char)).toBe(rot13Char)
+    describe("convert chars in the first half of the alphabet", () => {
+        it.each([
+            ['a', 'n'],
+            ['b', 'o'],
+            ['c', 'p'],
+        ])('char %s in %s', (char, rot13Char) => {
+            expect(new Rot13().convert(char)).toBe(rot13Char)
+        })
     })
 })
