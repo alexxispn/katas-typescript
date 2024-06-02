a48c86389db2c14327b0ebfc8f80173af1421dd1
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-06-02 12:15:40 +0100

    🟢green: converts char 'n' in 'a'

--- src/rot13/Rot13.ts
+++ src/rot13/Rot13.ts
@@ -1,6 +1,9 @@
 export class Rot13 {
     convert(char: string) {
         const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+        if(char === 'n') {
+            return 'a'
+        }
         return alphabet[alphabet.indexOf(char) + 13]
     }
 }
