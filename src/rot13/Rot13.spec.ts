3e0a42f070e0ba91151aa432eadd7a00343d12ff
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-06-02 12:02:08 +0100

    🔴red: converts char 'b' in 'o'

--- src/rot13/Rot13.spec.ts
+++ src/rot13/Rot13.spec.ts
@@ -4,4 +4,8 @@ describe("Rot13", () => {
     it("converts char 'a' in 'n'", () => {
         expect(new Rot13().convert("a")).toBe("n")
     })
+
+    it("converts char 'b' in 'o'", () => {
+        expect(new Rot13().convert("b")).toBe("o")
+    })
 })
